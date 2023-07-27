c023c8f53a3d11430b053bbc56107a773b278d4b
yifun <101847254+YIFUNLIN@users.noreply.github.com>
2023-07-27 13:09:52 +0800

    Update trash_bot.py

--- trash_bot.py
+++ trash_bot.py
@@ -228,5 +228,7 @@ def recognize(img):
     return reply_message,image_url
 
 
+import os
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
